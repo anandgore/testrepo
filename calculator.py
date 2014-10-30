e65f42ee48d78807e2e9f08c01f219d18ccbc769
anandgore <anandgore@users.noreply.github.com>
2014-10-30 16:27:25 -0700

    Python command line calculator
    Python command line calculator

---
+++ calculator.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+global e_e
+while True:
+	try:
+		rl=sys.stdin.readline()
+		e_e=eval(rl);
+	except SyntaxError as s:
+		os.system(rl)
+	except ValueError as v:
+		#print(v)
+		print("Ignoring input " + rl)
+	except NameError as n:
+		try:
+			e_e=eval("0x"+rl);
+		except NameError as n1:
+			os.system(rl)
+		except SyntaxError as s:
+			os.system(rl)
+		#print("Ignoring input " + rl)
+		#print(n)
+	try:
+		evaluated=int(e_e)
+		print( str(e_e) + " " + str(evaluated) + " " + str(bin(evaluated)) + " " 
+			+ str(hex(evaluated)) + " " 
+			+ str(oct(evaluated)))
+	except TypeError as t:
+		#e_e(100)
+		print(t)
+		pass
